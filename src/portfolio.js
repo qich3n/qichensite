ef054fd3b85fe465001250d045c4d09f11859866
qich3n <chen.qi3@northeastern.edu>
2024-08-16 04:55:35 -0400

    small change

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Full Stack Software Developer 🚀."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1gBl0rMe4L2MiWaFtbthn732i0hYikDyY/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
